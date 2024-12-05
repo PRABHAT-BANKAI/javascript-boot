ba0e0965c68cd48055c6aaf749244f8e9ca0201d
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2024-12-05 18:54:24 +0530

    datatypes

---
+++ day1/index.js
@@ -0,0 +1,14 @@
+console.log("hello world")
+
+console.log("one2234")
+
+document.write("<h1>hi from document write</h1>")
+
+// alert("hi from alert")
+// document.write
+// alert
+// Inner HTML
+
+
+let text = document.getElementById("text")
+text.innerText = "<h1>Hello world</h1>" //innerHTML  can we write html tag directly // innerText
---
+++ day2/index.js
@@ -0,0 +1,71 @@
+// what is variables
+// understanding data types
+// Types Of Operator
+// Arithmetic operators
+// Assignment operators
+// Comparison operators
+// Logical operators
+// conversion with console.log and innerHTML
+
+// let var const
+
+// javascript is a dynamic type language
+let number = 1234;
+number = "hello world";
+number = 1236659;
+
+//data types in javascript
+
+// primitive types
+// number
+let num = 123;
+console.log(typeof typeof num);
+// string
+
+let string = "batman";
+console.log(typeof typeof string);
+// boolean //true, false
+let boolean = true;
+console.log(typeof boolean);
+// bigInt //11112333323546547
+let value = 1234509823409802938509286098243098324n;
+console.log(typeof value);
+console.log(value);
+// null
+let userName = null;
+console.log(typeof userName);
+// undefined
+
+let game;
+console.log(typeof game);
+//not primitive types (reference type)
+// Object
+let object = { name: "batman", city: "gotham city" };
+console.log(object.city,object.name)
+console.log(object["name"])
+console.log(typeof object)
+// Array
+
+let array = ["clothes","fruits",12345,true,false,null,{name:"robin"},["food"]]
+
+
+let game1 = "pubg"
+let game2 = "callOfDuty"  // callOfDuty is camelCase   // snakeCase  user_name
+let game3 = "freefire"
+
+let arrayOfGroup = [game1,game2,game3]
+console.log(arrayOfGroup)
+console.log(typeof arrayOfGroup)
+
+// function
+//normal function 
+function print(){
+  console.log("hello world")
+}
+print()
+
+
+{
+  // block 
+}
+
